4d3ba76b688be59e7018f1c5692b58909a34f294
Rajesh Satpathy <rajesh.a.s.1997@gmail.com>
2019-04-28 03:10:00 +0530

    Added the rules for the game

--- rand.py
+++ rand.py
@@ -2,6 +2,10 @@ import random
 from datetime import datetime
 random.seed(datetime.now())
 
-for i in range(10):
-    k = random.randint(0,2)
-    print(k)
+M = 10
+N = 10
+wMap = [[0] * M] * N
+print(wMap)
+wMap[1][2] = 'a'
+print('-----------')
+print(wMap)
